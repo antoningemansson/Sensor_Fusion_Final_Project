9c0b45dd2ba70efa93e2e5ba6663747ce6a49e59
alelindv <81710560+alelindv@users.noreply.github.com>
2021-05-20 09:43:37 +0200

    Add files via upload

--- Matlab/tu_qw.m
+++ Matlab/tu_qw.m
@@ -2,7 +2,8 @@ function [x, P] = tu_qw(x_hat, P_hat, omega, T, Rw)
     
     F = eye(size(x_hat,1)) + 1/2*Somega(omega)*T;
     G = 1/2*Sq(x_hat)*T;
+    Q = T*Rw;
     
     x = F*x_hat;
-    P = F*P_hat*transpose(F) + G*Rw*transpose(G);
+    P = F*P_hat*transpose(F) + G*Q*transpose(G);
 end
--- Matlab/tu_qw_no_omega.m
+++ Matlab/tu_qw_no_omega.m
@@ -1,10 +1,12 @@
-function [x, P] = tu_qw_no_omega(x_hat, P_hat, Q)
+function [x, P] = tu_qw_no_omega(x_hat, P_hat, T, Rw)
     
 %     G = 1/2*Sq(x_hat)*T; %Is Sq also dependent on w?? Should it be taken away?
 %      G taken away since vk is only process noise of omega, without omega
 %      no vk either. Without vk, Rw does not exist, so some other noise is
 %      needed
     
+    Q = T*Rw;
+
     x = x_hat;
     P = P_hat + Q;
 end
